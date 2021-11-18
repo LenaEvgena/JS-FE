9a86770861f601293ab77ed16d28d257be9f75bd
Alena Astravukh <helen151204@gmail.com>
2021-11-18 15:43:49 +0300

    feat: add 9th solution

--- js/fundamentals.js
+++ js/fundamentals.js
@@ -133,11 +133,15 @@
 
 
 // 9. Write a function min(a,b) which returns the least of two numbers a and b:
-// min(2, 5) == 2
-// min(3, -1) == -1
-// min(1, 1) == 1
+// min(2, 5) //== 2
+// min(3, -1) //== -1
+// min(1, 1) //== 1
 {
+  function min(a, b) {
+    return Math.min(a, b);
+  }
 
+  min(2, 5); // == 2
 }
 
 
