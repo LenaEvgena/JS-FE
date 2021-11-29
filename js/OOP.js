7ceaacb3ca9a07cd893b06932d07bc92196998d8
Alena Astravukh <helen151204@gmail.com>
2021-11-29 11:32:06 +0300

    feat: add 2nd solution

--- js/OOP.js
+++ js/OOP.js
@@ -89,15 +89,36 @@ a.equals(b);
 // var john = myNew(Person, 'John', 30); // should work the same as:
 // var john = new Person('John', 30);
 
-// //Note: use the code below for your task:
-// function Person(name, age) {
-//     this.name = name;
-// 	this.age = age;
-// }
-// Person.prototype.introduce = function(){
-//     return 'My name is ' + this.name + ' and I am ' + this.age;
-// };
-// var john = new Person('John', 30);
-// var jack = new Person('Jack', 40);
-// console.log(john.introduce()); //My name is John and I am 30
-// console.log(jack.introduce()); //My name is Jack and I am 40
+//Note: use the code below for your task:
+function Person(name, age) {
+  this.name = name;
+	this.age = age;
+}
+
+Person.prototype.introduce = function(){
+    return 'My name is ' + this.name + ' and I am ' + this.age;
+};
+
+var john = new Person('John', 30);
+var jack = new Person('Jack', 40);
+
+console.log(john.introduce()); //My name is John and I am 30
+console.log(jack.introduce()); //My name is Jack and I am 40
+
+
+function myNew(Constructor, ...args) {
+  const instance = Object.create(Constructor.prototype);//новый объект, задали прототип с методами и свойством constructor
+  const result = instance.constructor(...args); //Мы можем использовать свойство constructor существующего объекта для создания нового с параметрами.
+
+
+  if (typeof result === 'object') { //если это объект - возвращаем этот объект явно
+    return result;
+  } else {
+    return instance;
+  }
+
+}
+
+let pete = myNew(Person, 'Pete', 30);
+console.log(pete.introduce()); //My name is Pete and I am 30
+
