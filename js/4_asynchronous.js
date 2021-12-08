c95dd308361075bc8b21985caec31b4909103ce0
Alena Astravukh <helen151204@gmail.com>
2021-12-08 23:44:20 +0300

    feat: add 1st solution

--- js/4_asynchronous.js
+++ js/4_asynchronous.js
@@ -10,6 +10,30 @@
 // })
 // console.log(4);
 // //1, 2, 4, 3
+class MyPromise {
+  constructor(fn) {
+    this.promise = new Promise(fn); //возвращаем новый промис с переданным коллбэком
+  }
+  then(fn) {
+    this.promise.then(fn); //используем метод then для промиса с переданным коллбэком (ставит в очередь микрозадач)
+    return this; //возвращаем this для чейнинга
+  }
+  synchThen(fn) {
+    fn(); // обычный вызов переданной функции (обычный синхронный код)
+    return this; //возвращаем this для чейнинга
+  }
+}
+
+
+let promise = new MyPromise((resolve) => {
+  console.log(1);
+  resolve();
+}).synchThen(()	=> {
+  console.log(2);
+}).then(() => {
+  console.log(3);
+})
+console.log(4);
 
 
 // 2. Write ReversePromise class so that ‘then’ functions are calling from the end to the start
