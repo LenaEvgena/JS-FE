b5762602952c027bde1f49eb162de32c1a1d39f4
Alena Astravukh <helen151204@gmail.com>
2021-12-13 13:00:23 +0300

    feat: add event listener to the container

--- js/5_events.js
+++ js/5_events.js
@@ -23,12 +23,45 @@ function createTable(rows, cells) {
 document.querySelector('.wrapper').appendChild( createTable(100, 100) );
 
 // add event listeners to each cell
-let cell = Array.from(document.querySelectorAll('td'));
-cell.forEach(cell => {
-  cell.addEventListener('click', (e) => {
-    console.log(`${e.target.textContent} cell clicked!`);
-  });
-})
+let cells = Array.from(document.querySelectorAll('td'));
+// cells.forEach(cell => {
+//   cell.addEventListener('click', () => {
+//     console.log(`${cell.textContent} cell clicked!`);
+//   });
+// })
+/* за 5 сек
+130 ms Scripting
+202 ms Rendering
+49 ms Painting
+124 ms System
+608 ms Idle
+
+141 ms Scripting
+224 ms Rendering
+48 ms Painting
+145 ms System
+707 ms Idle
+*/
+
+
+// add event listeners to the container
+let table = document.querySelector('table');
+table.addEventListener('click', (e) => {
+  console.log(`${e.target.textContent} cell of the table clicked!`);
+});
+/* за 5 сек
+126 ms Scripting
+201 ms Rendering
+46 ms Painting
+128 ms System
+613 ms Idle
+
+120 ms Scripting
+202 ms Rendering
+47 ms Painting
+125 ms System
+806 ms Idle
+*/
 
 
 // 2. Apply drag&drop functionality to each cell.
