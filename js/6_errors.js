d294328fa104c3575edbc091e7dcd71b1397cd83
Alena Astravukh <helen151204@gmail.com>
2021-12-14 20:18:15 +0300

    feat: add 1st solution

--- js/6_errors.js
+++ js/6_errors.js
@@ -1,5 +1,20 @@
 // 1. Create your custom error and trigger it.
 
+class MyError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'MyError';
+    this.message = message || 'Сообщение по умолчанию';
+  }
+}
+
+try {
+  throw new MyError('Ошибка');
+} catch (e) {
+  console.log(e.name);
+  console.log(e.message);
+}
+
 
 // 2. Choose 3 sites and analyze it using DevTools console:
 // - Do they have memory leaks?
